f624d8b2641e768c92b343320d0de04baa5b3c11
mehak94 <mehakkohli750@gmail.com>
2024-09-09 08:29:05 +0530

    This is my comment push

--- src/sept/ex_04092024/LAB006.java
+++ src/sept/ex_04092024/LAB006.java
@@ -3,6 +3,6 @@ package sept.ex_04092024;
 public class LAB006
 {
     public static void main(String[] args) {
-        System.out.println(1234hgfiuegfh6);
+        System.out.println(1234);
     }
 }
---
+++ src/sept/ex_09092024/Lab007.java
@@ -0,0 +1,15 @@
+package sept.ex_09092024;
+
+public class Lab007 {
+    public static void main(String[] args) {
+
+
+        //This is the single line comment
+        //It will not be executed
+    /*This is multiple line comment
+    Today was 4th lecture on Java
+     */
+        System.out.println (65);
+
+    }
+}
