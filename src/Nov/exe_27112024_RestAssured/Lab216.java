6fea4506be0333f6e7b3aa33402ad7fb7e4f2952
mehak94 <mehakkohli750@gmail.com>
2024-11-30 18:33:33 +0530

    All the files

---
+++ src/Nov/exe_27112024_RestAssured/Lab216.java
@@ -0,0 +1,21 @@
+package Nov.exe_27112024_RestAssured;
+
+public class Lab216 {
+    public static void main(String[] args) {
+
+        // Maven
+        // It is an open _ source build automation and project management tool.
+        // We can develop java based (enterprise) applications using it like :- j2ee - app  of SBI, springboot apps :- api
+        // We will use Maven for automation framework _ quickstart -. Maven template.
+        // Maven simplifies the process of managing dependencies , building projects and maintaining project structures
+        // through a standardised approach.
+        // Maven operates based on the project Object Model (POM) , pom.xml
+        // Maven can help us in :-
+        // Builds, Dependencies, Documentation, Reporting.
+
+        // How to use Maven?
+        // Install it and download Maven.( by default it s present in our intellij)
+        // file, new, project there is a option of maven, if maven is not there follow the following steps:-
+        // Install chocolatey ( we can install so many things in windows using it)
+    }
+}
